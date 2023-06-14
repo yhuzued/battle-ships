3bf517fc4a173fa8d830c378a9e3ec965ea533df
yhuzued <yusufmait@gmail.com>
2023-06-14 10:49:00 +0700

    Implement logic to determine move is legal

--- src/model/player.js
+++ src/model/player.js
@@ -4,3 +4,11 @@ export default function Player(name) {
   this.name = name;
   this.board = new GameBoard();
 }
+
+Player.prototype.shotOponent = function shotOponent(opponentBoard, coordinate) {
+  opponentBoard.receiveAttack(coordinate);
+};
+
+Player.prototype.shotIsLegal = function shotIsLegal(opponentBoard, coordinate) {
+  return opponentBoard.tiles.includes(coordinate);
+};
