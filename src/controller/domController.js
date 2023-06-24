837adfb3f3987039b3e5a7b89ce033e507e76f8f
yhuzued <yusufmait@gmail.com>
2023-06-24 10:02:35 +0700

    Color tile where ship is placed

--- src/controller/domController.js
+++ src/controller/domController.js
@@ -2,7 +2,7 @@ function changeBackground(array) {
   array.forEach((element) => {
     const el = document.querySelector(`#player-${element}`);
     if (el) {
-      el.classList.add('bg-blue-400');
+      el.classList.add('bg-blue-100');
     }
   });
 }
@@ -10,6 +10,10 @@ function changeBackground(array) {
 function removeBackground(color) {
   const tiles = document.querySelectorAll('.tile');
   tiles.forEach((element) => {
+    if (element.classList.contains('ship')) {
+      return;
+    }
+
     if (element.classList.contains(color)) {
       element.classList.remove(color);
     }
@@ -23,8 +27,19 @@ function colorRed(coordinate) {
   }
 }
 
+function tileWithShip(playerShips) {
+  const allCoordinate = playerShips.flatMap((ship) => ship.coordinate);
+  allCoordinate.forEach((coordinate) => {
+    const tile = document.querySelector(`#player-${coordinate}`);
+    if (tile) {
+      tile.classList.add('bg-blue-400');
+    }
+  });
+}
+
 export {
   changeBackground,
   removeBackground,
   colorRed,
+  tileWithShip,
 };
